6f1d2d782bd63c8055552ad469b9da401230a97b
Gabriela Ceranto <gabiceranto@gmail.com>
2023-01-16 18:55:31 -0300

    Voltando um commit

--- login.method.js
+++ login.method.js
@@ -1,58 +1,42 @@
-var mysql = require('mysql');
-const express = require('express');
-const bodyparser = require('body-parser');
-var app = express();
-
 
  const mysql = require('mysql2'); 
  
-function logar(event){
-  event.preventDefault();
-
-  app.use(bodyparser.json());
+ function logar(event){
+   event.preventDefault();
+  
  
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-  });// I believe this is a middleware function
-
-  const connection = mysql.createConnection({
-    host: 'connectada.made4it.com.br',
-    user: 'ada',
-    password: '1122334455',
-    database: 'connectada'
-  });
-  
-  
-    const username = document.getElementById("login").value;
-    const password = document.getElementById("password").value;
-  
-
-    
-    app.get('/listings', (req, res) => {
-    connection.query(
-      'SELECT * FROM `adacheck` WHERE `username` = ? AND `password` = ?',
-      [username, password],
-      function(err, results) {
-        if (results.length > 0)
-          alert("Entrou")
-          else 
-          alert("erro")
-      }
-    );
-    
-    connection.end();
-    })
-  };
-  
-  
+     
    
-  const buttonLogin = document.getElementById('buttonLogin')
-  buttonLogin.addEventListener("click", function(event){logar(event)});
-  
-
-  
+     const username = document.getElementById("login").value;
+     const password = document.getElementById("password").value;
+   
+ 
+     const connection = mysql.createConnection({
+       host: 'connectada.made4it.com.br',
+       user: 'ada',
+       password: '1122334455',
+       database: 'connectada'
+     });
+     
+     
+     connection.query(
+       'SELECT * FROM `adacheck` WHERE `username` = ? AND `password` = ?',
+       [username, password],
+       function(err, results) {
+         if (results.length > 0)
+           alert("Entrou")
+           else 
+           alert("erro")
+       }
+     );
+     connection.end();
+   
+   };
+   
+   
+    
+   const buttonLogin = document.getElementById('buttonLogin')
+   buttonLogin.addEventListener("click", function(event){logar(event)});
+   
+ 
+   
